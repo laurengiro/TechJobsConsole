54911838a8f007f2faf81fea79a7cddcb45ff40b
Lauren Girolmetti <laurengiro@gmail.com>
2019-10-31 19:56:53 -0400

    Made search methods case-insensitive

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -47,9 +47,9 @@ namespace TechJobsConsole
 
             foreach (Dictionary<string, string> row in AllJobs)
             {
-                string aValue = row[column];
+                string aValue = row[column].ToLower();
 
-                if (aValue.Contains(value))
+                if (aValue.Contains(value.ToLower()))
                 {
                     jobs.Add(row);
                 }
@@ -72,9 +72,9 @@ namespace TechJobsConsole
                 {
                     if (!foundJob)
                     {
-                        string aValue = row[column];
+                        string aValue = row[column].ToLower();
 
-                        if (aValue.Contains(value))
+                        if (aValue.Contains(value.ToLower()))
                         {
                             jobs.Add(row);
                             foundJob = true;
